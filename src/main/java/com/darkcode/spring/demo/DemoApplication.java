9960150e15bfd439ef47e7b0a7dc8bdd4adb7c2d
DavidChan015 <202303947@estudiante.udv.edu.gt>
2026-04-08 19:33:57 -0600

    prueba1.6

--- src/main/java/com/darkcode/spring/demo/DemoApplication.java
+++ src/main/java/com/darkcode/spring/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.darkcode.spring.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
+@RestController
+public class HelloController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @GetMapping("/")
+    public String holaMundo() {
+        return "Hola Mundo 3";
     }
 }
+

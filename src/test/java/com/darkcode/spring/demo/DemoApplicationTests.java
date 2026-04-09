4094218fe900a857d1da9df30d5a3b41a9db7bbe
DavidChan015 <202303947@estudiante.udv.edu.gt>
2026-04-08 19:38:58 -0600

    prueba1.8

--- src/test/java/com/darkcode/spring/demo/DemoApplicationTests.java
+++ src/test/java/com/darkcode/spring/demo/DemoApplicationTests.java
@@ -1,31 +1,20 @@
 package com.darkcode.spring.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class DemoApplicationTests {
-
-	@Autowired
-	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void holaMundoRespondeOk() throws Exception {
-		mockMvc.perform(get("/"))
-			.andExpect(status().isOk())
-			.andExpect(content().string("Hola Mundo 3"));
+	void holaMundoRespondeTextoEsperado() {
+		HelloController controller = new HelloController();
+		assertEquals("Hola Mundo 3", controller.holaMundo());
 	}
 
 }

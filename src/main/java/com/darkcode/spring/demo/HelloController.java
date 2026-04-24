40776cab2fe44cb0e1dec045dfa6bcf2ad0e3b0b
DavidChan015 <202303947@estudiante.udv.edu.gt>
2026-04-23 20:53:55 -0600

    prueba1.3

--- src/main/java/com/darkcode/spring/demo/HelloController.java
+++ src/main/java/com/darkcode/spring/demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    //hola3
+    //hola2
     @GetMapping("/")
     public String holaMundo() {
         return "Hola Mundo 2";
